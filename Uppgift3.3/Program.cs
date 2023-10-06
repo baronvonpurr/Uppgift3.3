e1dbff6c2ddce29af29742d229ebdefe20b42786
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-06 13:20:20 +0200

    Add project files.

---
+++ Uppgift3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+            int maxpris = timmar * 80;
+            
+            if (maxpris <= 950)
+            {
+                Console.WriteLine("Det kommer kosta " +  maxpris + "Kr");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Det kostar " + (maxpris - 950) + "kr för mycket!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
